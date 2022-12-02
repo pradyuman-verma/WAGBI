018e7fc88a3081d061bd3efb0a5a9f2e2de06ac9
pradyuman-verma <pradyumnverma27@gmail.com>
2022-12-02 18:47:43 +0530

    hardhat init
    Signed-off-by: pradyuman-verma <pradyumnverma27@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+> ॐ नमः शिवाय
+
+# 0xAlpha's ETH India Project
---
+++ hardhat.config.ts
@@ -0,0 +1,110 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-web3";
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
+import "@typechain/hardhat";
+import { resolve } from "path";
+import { config as dotenvConfig } from "dotenv";
+import bigNumber from "bignumber.js";
+
+dotenvConfig({ path: resolve(__dirname, "./.env") });
+
+const chainIds = {
+  ganache: 1337,
+  hardhat: 31337,
+  mainnet: 1,
+  avalanche: 43114,
+  polygon: 137,
+  arbitrum: 42161,
+  optimism: 10,
+  fantom: 250,
+};
+
+const alchemyApiKey = process.env.ALCHEMY_API_KEY;
+if (!alchemyApiKey) {
+  throw new Error("Please set your ALCHEMY_API_KEY in a .env file");
+}
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const mnemonic =
+  process.env.MNEMONIC ??
+  "test test test test test test test test test test test junk";
+
+const networkGasPriceConfig: Record<string, number> = {
+  polygon: 50,
+  mumbai: 300,
+};
+
+function createConfig(network: string) {
+  return {
+    url: getNetworkUrl(network),
+    accounts: !!PRIVATE_KEY ? [`0x${PRIVATE_KEY}`] : { mnemonic },
+    gasPrice: new bigNumber(networkGasPriceConfig[network])
+      .multipliedBy(1e9)
+      .toNumber(), // Update the mapping above
+  };
+}
+
+function getNetworkUrl(networkType: string) {
+  if (networkType === "goerli")
+    return `https://eth-goerli.g.alchemy.com/v2/${alchemyApiKey}`;
+  else if (networkType === "mumbai")
+    return `https://polygon-mumbai.g.alchemy.com/v2/${alchemyApiKey}`;
+  else return `https://eth-goerli.g.alchemy.com/v2/${alchemyApiKey}`;
+}
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic,
+      },
+      chainId: chainIds.hardhat,
+      forking: {
+        url: String(getNetworkUrl(String(process.env.networkType))),
+        blockNumber: 8031539, // goerli
+      },
+    },
+    mumbai: createConfig("mumbai"),
+    polygon: createConfig("polygon"),
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  etherscan: {
+    apiKey: {
+      polygon: String(process.env.POLY_ETHSCAN_KEY),
+    },
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
+  mocha: {
+    timeout: 10000 * 1000, // 10,000 seconds
+  },
+};
+
+export default config;

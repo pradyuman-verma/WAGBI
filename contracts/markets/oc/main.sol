0a09532f5aebd5cee1261fab1b225d40761fdb54
Vaibhav Khanna <vaibhavkhanna78@gmail.com>
2022-12-03 05:14:45 +0530

    add storage variables

--- contracts/markets/oc/main.sol
+++ contracts/markets/oc/main.sol
@@ -8,7 +8,15 @@ contract OCMarket {
 
     uint256 internal _status;
 
-    mapping(address => mapping(address => uint256)) public userData;
+    // User Supply and Borrow amounts data
+    // user => token => uint
+    // 2 things stored in a single uint:-
+    // First 59 bits => 0-58 => user raw supply (raw supply = totalSupply / supplyExchangePrice)
+    // Next 58 bits => 59-116 => user raw borrow (raw borrow = totalBorrow / borrowExchangePrice)
+    // Last 139 bits blank
+    mapping(address => mapping(address => uint256)) public userAmountsData;
+
+    mapping(address => uint256) public userTokensData; // TODO: This will store in which tokens a user supplied in bits
 
     modifier nonReentrant() {
         require(_status == 1, "ReentrancyGuard: reentrant call");

460196da9b73708361dabc8f6d7a9dd382ad2471
Vaibhav Khanna <vaibhavkhanna78@gmail.com>
2022-12-02 20:31:23 +0530

    add basic layout

--- contracts/liquidity-pool/main.sol
+++ contracts/liquidity-pool/main.sol
@@ -3,4 +3,94 @@ pragma solidity ^0.8.0;
 
 import "./helpers.sol";
 
-contract LiquidityPool is Helpers {}
+contract Internals is Helpers {
+    function supplyInternal(
+        address token_,
+        uint256 amount_,
+        address from_
+    ) internal {
+        IERC20(token_).safeTransferFrom(from_, address(this), amount_);
+        // update user related storage
+        // event
+    }
+
+    function withdrawInternal(
+        address token_,
+        uint256 amount_,
+        address to_
+    ) internal {
+        // update user related storage
+        IERC20(token_).safeTransfer(to_, amount_);
+        // event
+    }
+
+    function borrowInternal(
+        address token_,
+        uint256 amount_,
+        address to_
+    )
+        internal
+        returns (
+            uint256 oldRawAmount_,
+            uint256 newRawAmount_,
+            uint256 supplyExchangePrice_,
+            uint256 borrowExchangePrice_
+        )
+    {
+        // update user related storage
+        IERC20(token_).safeTransfer(to_, amount_);
+        // event
+    }
+
+    function paybackInternal(
+        address token_,
+        uint256 amount_,
+        address from_
+    )
+        internal
+        returns (
+            uint256 oldRawAmount_,
+            uint256 newRawAmount_,
+            uint256 supplyExchangePrice_,
+            uint256 borrowExchangePrice_
+        )
+    {
+        IERC20(token_).safeTransferFrom(from_, address(this), amount_);
+        // update user related storage
+        // event
+    }
+}
+
+contract LiquidityPool is Internals {
+    function supply(
+        address token_,
+        uint256 amount_,
+        address from_
+    ) public {
+        supplyInternal(token_, amount_, from_);
+    }
+
+    function withdraw(
+        address token_,
+        uint256 amount_,
+        address to_
+    ) public {
+        withdrawInternal(token_, amount_, to_);
+    }
+
+    function borrow(
+        address token_,
+        uint256 amount_,
+        address to_
+    ) public {
+        borrowInternal(token_, amount_, to_);
+    }
+
+    function payback(
+        address token_,
+        uint256 amount_,
+        address from_
+    ) public {
+        paybackInternal(token_, amount_, from_);
+    }
+}

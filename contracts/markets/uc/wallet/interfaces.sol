c8a452925f50ed9a62035afc07ac694d9a4b8690
Vaibhav Khanna <vaibhavkhanna78@gmail.com>
2022-12-03 11:22:25 +0530

    add liquidity and oracle interfaces

--- contracts/markets/uc/wallet/interfaces.sol
+++ contracts/markets/uc/wallet/interfaces.sol
@@ -1,2 +1,77 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import {IERC20} from "../../../dependencies/IERC20.sol";
+
+interface ILiquidity {
+    function supply(
+        address token_,
+        uint256 amount_,
+        address from_
+    )
+        external
+        returns (
+            uint256 oldRawAmount_,
+            uint256 newRawAmount_,
+            uint256 supplyExchangePrice_,
+            uint256 borrowExchangePrice_
+        );
+
+    function withdraw(
+        address token_,
+        uint256 amount_,
+        address to_
+    )
+        external
+        returns (
+            uint256 oldRawAmount_,
+            uint256 newRawAmount_,
+            uint256 supplyExchangePrice,
+            uint256 borrowExchangePrice
+        );
+
+    function borrow(
+        address token_,
+        uint256 amount_,
+        address to_
+    )
+        external
+        returns (
+            uint256 oldRawAmount_,
+            uint256 newRawAmount_,
+            uint256 supplyExchangePrice_,
+            uint256 borrowExchangePrice_
+        );
+
+    function payback(
+        address token_,
+        uint256 amount_,
+        address from_
+    )
+        external
+        returns (
+            uint256 oldRawAmount_,
+            uint256 newRawAmount_,
+            uint256 supplyExchangePrice_,
+            uint256 borrowExchangePrice_
+        );
+
+    function getUserSupplyAmount(address user_, address token_)
+        external
+        view
+        returns (uint256 rawSupplyAmount_, uint256 supplyAmount_);
+
+    function getUserBorrowAmount(address user_, address token_)
+        external
+        view
+        returns (uint256 rawBorrowAmount_, uint256 borrowAmount_);
+
+    function getExchangePrices(address token_)
+        external
+        view
+        returns (uint256 supplyExchangePrice_, uint256 borrowExchangePrice_);
+}
+
+interface IOracle {
+    function getPriceInEth(address token_) external view returns (uint256);
+}

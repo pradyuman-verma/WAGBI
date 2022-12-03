20e0cc611f7e0dc9bcebc50e0a70d7d1a1065a74
Vaibhav Khanna <vaibhavkhanna78@gmail.com>
2022-12-03 06:50:43 +0530

    almost complete withdraw function

--- contracts/markets/oc/main.sol
+++ contracts/markets/oc/main.sol
@@ -24,8 +24,8 @@ contract OCMarket is Helpers {
     function supply(
         address token_,
         uint256 amount_,
-        address to_
-    ) external {
+        address for_
+    ) external nonReentrant {
         uint256 assetIndex_ = getAssetIndex(token_);
 
         (uint256 oldRawAmount_, uint256 newRawAmount_, , ) = LIQUIDITY.supply(
@@ -35,8 +35,8 @@ contract OCMarket is Helpers {
         );
 
         // update user amounts data
-        uint256 userAmountsData_ = userAmountsData[to_][token_];
-        userAmountsData[to_][token_] = pack(
+        uint256 userAmountsData_ = userAmountsData[for_][token_];
+        userAmountsData[for_][token_] = pack(
             userAmountsData_,
             unpack(userAmountsData_, 0, 58) + newRawAmount_ - oldRawAmount_,
             0,
@@ -44,12 +44,51 @@ contract OCMarket is Helpers {
         );
 
         // update user tokens data
-        uint256 userTokensData_ = userTokensData[to_];
+        uint256 userTokensData_ = userTokensData[for_];
         uint256 newUserTokensData_ = addToSupplyTokens(
             userTokensData_,
             assetIndex_
         );
         if (newUserTokensData_ != userTokensData_)
-            userTokensData[to_] = newUserTokensData_;
+            userTokensData[for_] = newUserTokensData_;
+    }
+
+    function withdraw(
+        address token_,
+        uint256 amount_,
+        address to_
+    ) external nonReentrant {
+        uint256 assetIndex_ = getAssetIndex(token_);
+
+        (uint256 oldRawAmount_, uint256 newRawAmount_, , ) = LIQUIDITY.withdraw(
+            token_,
+            amount_,
+            to_
+        );
+
+        // update user amounts data
+        uint256 userAmountsData_ = userAmountsData[msg.sender][token_];
+        uint256 userFinalSuppliedAmount_ = unpack(userAmountsData_, 0, 58) +
+            oldRawAmount_ -
+            newRawAmount_;
+        userAmountsData[msg.sender][token_] = pack(
+            userAmountsData_,
+            userFinalSuppliedAmount_,
+            0,
+            58
+        );
+
+        // update user tokens data
+        if (userFinalSuppliedAmount_ == 0) {
+            uint256 userTokensData_ = userTokensData[msg.sender];
+            uint256 newUserTokensData_ = removeFromSupplyTokens(
+                userTokensData_,
+                assetIndex_
+            );
+            if (newUserTokensData_ != userTokensData_)
+                userTokensData[to_] = newUserTokensData_;
+        }
+
+        // TODO: check hf
     }
 }

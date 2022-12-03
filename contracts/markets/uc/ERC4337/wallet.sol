76be6c733c8cdae9925dfefa95ee851939f3f557
pradyuman-verma <pradyumnverma27@gmail.com>
2022-12-03 23:39:40 +0530

    ERC4337
    Signed-off-by: pradyuman-verma <pradyumnverma27@gmail.com>

---
+++ contracts/markets/uc/ERC4337/wallet.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "../../../dependencies/ERC4337/ERC4337.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract WagbiWallet is ERC4337 {
+    using ECDSA for bytes32;
+
+    uint256 public wNonce;
+
+    IEntryPoint public immutable ENTRY_POINT;
+
+    // Domain Seperators
+    // keccak256(
+    //     "EIP712Domain(uint256 chainId,address verifyingContract)"
+    // );
+    bytes32 internal constant DOMAIN_SEPARATOR_TYPEHASH =
+        0x47e79534a245952e8b16893a336b85a3d9ea9fa8c573f3d803afb92a79469218;
+
+    // keccak256(
+    //     "WalletTx(address to,uint256 value,bytes data,uint8 operation,uint256 targetTxGas,uint256 baseGas,uint256 gasPrice,address gasToken,address refundReceiver,uint256 nonce)"
+    // );
+    bytes32 internal constant WALLET_TX_TYPEHASH =
+        0xeedfef42e81fe8cd0e4185e4320e9f8d52fd97eb890b85fa9bd7ad97c9a18de2;
+
+    // authority
+    address public auth;
+
+    modifier onlyOwner() {
+        require(msg.sender == auth, "NOT-AUTH");
+        _;
+    }
+
+    modifier onlyEntryPoint() {
+        require(msg.sender == address(ENTRY_POINT), "not-entry-point");
+        _;
+    }
+
+    function nonce() public view virtual override returns (uint256) {
+        return wNonce;
+    }
+
+    function entryPoint() public view virtual override returns (IEntryPoint) {
+        return ENTRY_POINT;
+    }
+
+    // over-riding Parent contract existing method
+    function _validateAndUpdateNonce(UserOperation calldata userOp_)
+        internal
+        override
+    {
+        require(wNonce++ == userOp_.nonce, "wallet: invalid nonce");
+    }
+
+    function _payPrefund(uint256 requiredPrefund_) internal override {
+        if (requiredPrefund_ != 0)
+            payable(msg.sender).call{
+                value: requiredPrefund_,
+                gas: type(uint256).max
+            }("");
+    }
+
+    function _validateSignature(
+        UserOperation calldata userOp_,
+        bytes32 data_,
+        address
+    ) internal view override {
+        require(
+            auth == data_.toEthSignedMessageHash().recover(userOp_.signature) ||
+                tx.origin == address(0),
+            "wrong-sig"
+        );
+    }
+
+    function setOwner(address newOwner_) external onlyOwner {
+        require(newOwner_ != address(0), "zero-owner");
+        auth = newOwner_;
+    }
+
+    function domainSeparator() public view returns (bytes32) {
+        return
+            keccak256(
+                abi.encode(DOMAIN_SEPARATOR_TYPEHASH, getChainId(), this)
+            );
+    }
+
+    function getChainId() public view returns (uint256) {
+        uint256 id_;
+        assembly {
+            id_ := chainid()
+        }
+        return id_;
+    }
+
+    constructor(address owner_, address entry_) payable {
+        auth = owner_;
+        ENTRY_POINT = IEntryPoint(payable(entry_));
+    }
+
+    receive() external payable {}
+}

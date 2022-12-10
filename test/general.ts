1cea9e99defb1ba763627cfeac8741e56a8a0075
Vaibhav Khanna <vaibhavkhanna78@gmail.com>
2022-12-11 00:53:58 +0530

    start general tests

--- test/general.ts
+++ test/general.ts
@@ -1,4 +1,6 @@
+import { Contract } from "ethers";
 import { ethers } from "hardhat";
+const { expect } = require("chai");
 
 describe("General", function () {
   const aaveV2LendingPool = "0x4bd5643ac6f66a5237E18bfA7d47cF22f1c9F210";
@@ -8,7 +10,18 @@ describe("General", function () {
   const usdcAddr = "0x9FD21bE27A2B059a288229361E2fA632D8D2d074";
   const daiAddr = "0x75Ab5AB1Eef154C0352Fc31D2428Cef80C7F8B33";
   const wbtcAddr = "0xf4423F4152966eBb106261740da907662A3569C5";
+
+  let deployer, proxyAdmin: Contract;
   before(async () => {
-    
+    [deployer] = await ethers.getSigners();
+
+    const ProxyAdmin = await ethers.getContractFactory("OrbitProxyAdmin");
+    proxyAdmin = await ProxyAdmin.deploy(deployer.address);
+    await proxyAdmin.deployed();
+    console.log("Proxy Admin deployed at:", proxyAdmin.address);
+  });
+
+  it("should contracts deployed", async () => {
+    expect(!!proxyAdmin.address).to.equal(true);
   });
 });
